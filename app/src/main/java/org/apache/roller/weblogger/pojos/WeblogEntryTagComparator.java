64bae5e6f08fe7997110b154e05410929c636ee6
Michael Bien <mbien42@gmail.com>
2020-09-19 13:01:18 +0200

    removed redundant toString().

--- app/src/main/java/org/apache/roller/weblogger/pojos/WeblogEntryTagComparator.java
+++ app/src/main/java/org/apache/roller/weblogger/pojos/WeblogEntryTagComparator.java
@@ -41,7 +41,7 @@ public class WeblogEntryTagComparator implements Comparator<WeblogEntryTag>,
      */
     @Override
     public int compare(WeblogEntryTag o1, WeblogEntryTag o2) {
-        return o1.getName().toString().compareTo(o2.getName().toString());
+        return o1.getName().compareTo(o2.getName());
     }
 
 }
